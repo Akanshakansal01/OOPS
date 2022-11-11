972e661c5a94cefea1b26c8ae9b148ab5731f3e7
Akanshakansal01 <akanshakansal025@gmail.com>
2022-11-11 22:52:36 +0530

    new

---
+++ src/oops/Person_client.java
@@ -0,0 +1,22 @@
+package oops;
+
+public class Person_client {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello");
+        Person p = new Person();
+        System.out.println(p.name);
+        System.out.println(p.age);
+        p.name = "Kaju";
+        p.age = 21;
+        p.Intro_yourSelf();
+        Person p1 = new Person();
+        p1.name = "kamlesh";
+        p1.age = 18;
+        p1.Intro_yourSelf();
+        p.SayHey("Ram");
+        System.out.println(p1.count);
+        Person.count += 21;
+    }
+}
---
+++ src/oops/Student_client.java
@@ -0,0 +1,9 @@
+package oops;
+
+public class Student_client {
+    public static void main(String[] args) {
+        System.out.println("Hey");
+        Student s = new Student("Kaju", 21);
+
+    }
+}
